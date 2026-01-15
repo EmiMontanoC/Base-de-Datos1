25664f14dc1fdeeb4ccfcd74fc0ad4ae467f1514
Emiliano MC <emimontano1327@gmail.com>
2026-01-15 11:21:08 -0700

    modified:   Bases_de_datos/Datos_Asentamientos.java

--- Bases_de_datos/Datos_Asentamientos.java
+++ Bases_de_datos/Datos_Asentamientos.java
@@ -1,3 +1,4 @@
+package Bases_de_datos;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -23,3 +24,19 @@ public class Datos_Asentamientos {
         }
     }
 }
+
+
+
+//Estimacion de tiempo y notas
+//Tiempo estimado total: 80min
+// Investigar librerias para manejo de datos y CSV: 20min
+// Planeacion de la estructura semi-inicial del codigo: 15min
+// Escritura del codigo: 30min
+// Pruebas y correcciones 15min
+
+//Tiempo real total: 35min
+//Investigar librerias para manejo de datos y CSV: 10min
+//Planeacion de la estructura semi_inicial del codigo: 10 min
+//Escritura del codigo: 5min
+//pruebas y correcciones: 10min
+
